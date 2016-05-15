f5f17f111936c9dbc94760d9c0fbe958117da9b4
YI Peipei <yipeipei@gmail.com>
2016-05-15 22:56:14 +0800

    install bashrc

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ setup-git:
 
 setup-cygwin:
 	ln -s -f $(shell pwd)/cygwin/.minttyrc ~/.minttyrc
+
+setup-bash:
+	./bash/install-bashrc.sh
---
+++ bash/install-bashrc.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+bashrc="\"${DIR}/bashrc.sh\""
+rclist=(
+    ~/.bashrc
+    ~/.zshrc
+    )
+
+for rc in "${rclist[@]}"
+do
+    if [ -f ${rc} ]; then
+        if [[ $( grep ${bashrc}  ${rc}) ]]; then
+            echo Already installed ${bashrc} to ${rc}.
+        else
+            echo Installing ${bashrc} to ${rc}.
+            echo >> ${rc}
+            echo source ${bashrc} >> ${rc}
+            echo Done.
+        fi
+    fi
+done
