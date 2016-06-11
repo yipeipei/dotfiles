73538e52cd95529959a6b7e344eb0e1f843a06e2
Peipei YI <yipeipei@gmail.com>
2016-06-11 23:22:43 +0800

    link itunes files

---
+++ windows/link-itunes.cmd
@@ -0,0 +1,4 @@
+mklink /J "%USERPROFILE%\Music\iTunes\iTunes Media\Music" "%USERPROFILE%\Google Drive\Music\iTunes\iTunes Media\Music"
+mklink /H "%USERPROFILE%\Music\iTunes\iTunes Library.itl" "%USERPROFILE%\Google Drive\Music\iTunes\iTunes Library.itl"
+mklink /H "%USERPROFILE%\Music\iTunes\iTunes Library Extras.itdb" "%USERPROFILE%\Google Drive\Music\iTunes\iTunes Library Extras.itdb"
+mklink /H "%USERPROFILE%\Music\iTunes\iTunes Library Genius.itdb" "%USERPROFILE%\Google Drive\Music\iTunes\iTunes Library Genius.itdb"
