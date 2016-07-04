72b6b1b40dcdd01de30d1258dc7f96f882fa2204
YI Peipei <yipeipei@gmail.com>
2016-07-04 20:09:07 +0800

    global gitignore

--- git/git-config.sh
+++ git/git-config.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Clean
+> .gitconfig
+> .gitignore_global
+
+# Global gitignore
+curl https://www.gitignore.io/api/osx,linux,windows,xcode,visualstudio,jetbrains,eclipse,sublimetext,emacs,vim > .gitignore_global
+
 # First-Time Git Setup
 git config --global user.name "YI Peipei"
 git config --global user.email "yipeipei@gmail.com"
