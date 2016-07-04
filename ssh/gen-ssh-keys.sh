03434c0cd63d06423f1ad12d7c5e5561cb0fc5f7
Peipei YI <yipeipei@gmail.com>
2016-07-04 14:18:13 +0800

    separate keys

--- ssh/gen-ssh-keys.sh
+++ ssh/gen-ssh-keys.sh
@@ -11,10 +11,10 @@ devices=(
 
 for d in "${devices[@]}"
 do
-    if [ ! -f "id_rsa_${d}" ]; then
+    if [ ! -f "keys/id_rsa_${d}" ]; then
         echo "Generating a new SSH key for ${d}"
-        ssh-keygen -t rsa -b 4096 -C "Peipei on ${d}" -f "id_rsa_${d}"
+        ssh-keygen -t rsa -b 4096 -C "Peipei on ${d}" -f "keys/id_rsa_${d}"
     fi
-    chmod 600 "id_rsa_${d}"
-    chmod 644 "id_rsa_${d}.pub"
+    chmod 600 "keys/id_rsa_${d}"
+    chmod 644 "keys/id_rsa_${d}.pub"
 done
--- ssh/test_ssh_keys.sh
+++ ssh/test_ssh_keys.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for f in id_rsa_*.pub
+for f in keys/id_rsa_*.pub
 do
     echo "Challenge ${f%.pub}"
     ssh-keygen -y -f "${f%.pub}"
