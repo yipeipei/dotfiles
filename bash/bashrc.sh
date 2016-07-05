568054f3b676716df664eca3ddd56ae02472ec85
YI Peipei <yipeipei@gmail.com>
2016-07-05 13:51:56 +0800

    add the babun default

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
 
+# If not running interactively, don't do anything
+[[ "$-" != *i* ]] && return
+
+# Info
+ls -l $SHELL
+
+# Zsh
+if [ -n "$ZSH_VERSION" ]; then
+    export ZSH=$HOME/.oh-my-zsh
+    ZSH_THEME="babun"
+    plugins=(git)
+    export PATH=$HOME/bin:/usr/local/bin:$PATH
+    source $ZSH/oh-my-zsh.sh
+fi
