de3392fee8020b99e85181535f51bdb0d98f5b38
YI Peipei <yipeipei@gmail.com>
2016-07-05 12:47:07 +0800

    add safe ln

---
+++ ln.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Save the old file before making a symbolic link
+
+for last; do true; done
+if [ -e "${last}" ] && [ ! -L "${last}" ]; then
+    mv -v "${last}" "${last}.old"
+fi
+ln "$@"
