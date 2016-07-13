57765eb7e0d55900390656c50e5ec8f88477bd16
YI Peipei <yipeipei@gmail.com>
2016-07-13 21:24:25 +0800

    add python packages

---
+++ python/install-packages.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Install pip and other packages
+
+# Bootstrapping the pip installer
+python3 -m ensurepip --upgrade
+
+pip3 install --upgrade pip
+pip3 install --upgrade youtube-dl
