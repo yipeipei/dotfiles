9e23a6fc10d942042288e63cd465e3054beddaf7
YI Peipei <yipeipei@gmail.com>
2016-07-31 00:53:23 +0800

    add git branch to PS1

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -28,11 +28,17 @@ if [ -f ~/.git-completion.bash ]; then
     source ~/.git-completion.bash
 fi
 
+# Add git branch to bash prompt
 if [ -f ~/.git-prompt.sh ]; then
     source ~/.git-prompt.sh
 fi
 
-PS1='\[\e]0;\w\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\n\$ '
+RED="\[$(tput setaf 1)\]"
+GREEN="\[$(tput setaf 2)\]"
+YELLOW="\[$(tput setaf 3)\]"
+BLUE="\[$(tput setaf 4)\]"
+
+PS1="\[\e]0;\w\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w$GREEN\$(__git_ps1)\[\e[0m\]\n\$ "
 
 # History
 export HISTSIZE=1000000
