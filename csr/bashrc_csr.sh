328fba9e147ae9fcba832cfd5cc691a31a8d4fb1
YI Peipei <yipeipei@gmail.com>
2016-08-05 13:56:00 +0800

    add csrusage

--- csr/bashrc_csr.sh
+++ csr/bashrc_csr.sh
@@ -2,11 +2,19 @@
 # /opt/hadoop/lib/native/:
 unset LD_LIBRARY_PATH
 
-# Info
-quota -s
-hadoop fs -count -q -h "$HOME"
-free -h
-df -h /tmp
+# CSR usage
+function csrusage {
+    STATUS_HOME_USED="$(quota -s | tail -n 1 | tr -s ' ' | cut -d' ' -f3)"
+    STATUS_HDFS_USED="$(hadoop fs -count -q -h "$HOME" | tr -s ' ' | cut -d' ' -f10-11 --output-delimiter='')"
+    STATUS_MEM_FREE="$(free -h | head -n 2 | tail -n 1 | tr -s ' ' | cut -d' ' -f4)"
+    STATUS_TMP_FREE="$(df -h /tmp | tail -n 1 | tr -s ' ' | cut -d' ' -f4)"
+    echo home used: $STATUS_HOME_USED
+    echo hdfs used: $STATUS_HDFS_USED
+    echo mem free:  $STATUS_MEM_FREE
+    echo tmp free:  $STATUS_TMP_FREE
+}
+echo
+csrusage
 
 # Protect new file outside home
 umask 0077
