333bad3bca8ce24a459390811166be4a40989739
YI Peipei <yipeipei@gmail.com>
2016-08-10 19:40:17 +0800

    check existing agent; separate start agent, add key

--- ssh/auto-launch-ssh-agent.sh
+++ ssh/auto-launch-ssh-agent.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# https://help.github.com/articles/working-with-ssh-key-passphrases/#auto-launching-ssh-agent-on-git-for-windows
-
 env="$HOME/.ssh/agent.env.$(hostname)"
 
 agent_load_env () { test -f "$env" && . "$env" >| /dev/null ; }
@@ -10,15 +8,22 @@ agent_start () {
     (umask 077; ssh-agent >| "$env")
     . "$env" >| /dev/null ; }
 
-agent_load_env
-
 # agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2= agent not running
-agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
+agent_run_state () {
+    ssh-add -l >| /dev/null 2>&1; echo $?
+}
 
-if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
-    agent_start
-    ssh-add
-elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
+# if no existing (e.g. forwarded) agent, load from env
+if [[ "$(agent_run_state)" -eq 2 ]]; then
+    agent_load_env
+    # if failed to load from env, start agent
+    if [[ "$(agent_run_state)" -eq 2 ]]; then
+        agent_start
+    fi
+fi
+
+# if agent without key, add key
+if [[ "$(agent_run_state)" -eq 1 ]]; then
     ssh-add
 fi
 
