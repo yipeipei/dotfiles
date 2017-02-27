6b0f4330241adfe053230c7110a5d792d8bc4540
YI Peipei <yipeipei@gmail.com>
2017-02-27 19:48:37 +0800

    add psutil and pandas

--- python/pip3-install.sh
+++ python/pip3-install.sh
@@ -9,10 +9,12 @@ pip3 install --upgrade pipdeptree
 pip3 install --upgrade youtube-dl
 pip3 install --upgrade you-get
 pip3 install --upgrade pythonpy
+pip3 install --upgrade psutil
 pip3 install --upgrade scrapy
 pip3 install --upgrade flask
 pip3 install --upgrade requests
 pip3 install --upgrade numpy
 pip3 install --upgrade networkx
+pip3 install --upgrade pandas
 
 pip3 freeze > requirements.txt
