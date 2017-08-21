66a1a77315ceac2bf13768131d0e1bf78370f102
YI Peipei <yipeipei@gmail.com>
2017-08-21 17:18:25 +0800

    install via brew

---
+++ macos/brew-install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+! command -v brew && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install wget
+brew install python3
+brew install emacs
