b6139fa80db72a714dc1dd50d6b000cc47f25aa7
YI Peipei <yipeipei@gmail.com>
2017-08-21 17:27:41 +0800

    install via brew cask

---
+++ macos/brew-cask-install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+brew tap caskroom/cask
+
+brew cask install dash
+brew cask install skim
+brew cask install macpass
+brew cask install dropbox
+brew cask install ipe
+brew cask install appcleaner
+brew cask install basictex
+brew cask install quitter
+brew cask install iina
