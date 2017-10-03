5ac5c58a4512bf896fa8c72f9fa462a3a5a6478f
YI Peipei <yipeipei@gmail.com>
2017-10-03 21:09:22 +0800

    unify comment

--- ssh/gen-ssh-key.sh
+++ ssh/gen-ssh-key.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
 
-devices=(
-    "one"
-    "two"
-    "iPad"
-    "iPhone"
-    "MacBook"
-    "cs9170"
-    )
-
-for d in "${devices[@]}"
-do
-    if [ ! -f "keys/id_rsa_${d}" ]; then
-        echo "Generating a new SSH key for ${d}"
-        ssh-keygen -t rsa -b 4096 -C "Peipei on ${d}" -f "keys/id_rsa_${d}"
-    fi
-    chmod 600 "keys/id_rsa_${d}"
-    chmod 644 "keys/id_rsa_${d}.pub"
-done
+DATE_TIME="$(date +%Y-%m-%d\ %H:%M:%S\ %z)"
+ssh-keygen -t rsa -b 4096 -C "$USER@$HOSTNAME on $DATE_TIME"
