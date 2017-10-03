9c31eb084fc42ed3a09c71c6ba078f78f8d4a49b
YI Peipei <yipeipei@gmail.com>
2017-10-03 21:19:56 +0800

    fix tiny

--- ssh/gen-ssh-key.sh
+++ ssh/gen-ssh-key.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 DATE_TIME="$(date +%Y-%m-%d\ %H:%M:%S\ %z)"
-ssh-keygen -t rsa -b 4096 -C "$USER@$HOSTNAME on $DATE_TIME"
+ssh-keygen -t rsa -b 4096 -C "$USER@$HOSTNAME generated on $DATE_TIME"
