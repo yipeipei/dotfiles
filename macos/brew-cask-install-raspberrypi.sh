2d01e3b7c9c2be44a7e90e7b560085f6840ecf54
YI Peipei <yipeipei@gmail.com>
2017-10-04 12:30:33 +0800

    split brew cask install raspberrypi

---
+++ macos/brew-cask-install-raspberrypi.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+brew cask install sdformatter
+brew cask install etcher
--- macos/brew-cask-install.sh
+++ macos/brew-cask-install.sh
@@ -5,8 +5,6 @@ brew tap caskroom/cask
 brew cask install quitter
 brew cask install appcleaner
 brew cask install macpass
-brew cask install sdformatter
-brew cask install etcher
 brew cask install iina
 brew cask install dropbox
 brew cask install dash
