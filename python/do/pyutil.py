c0fdca3aae95f7c431c828f60999ef0243b163cf
YI Peipei <yipeipei@gmail.com>
2017-10-20 14:36:46 +0800

    extract update_dir

--- python/do/pyutil.py
+++ python/do/pyutil.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 # Standalone functions
+
+def update_dir(git_dir):
+    if not os.path.isdir(git_dir):
+        # exit if git_dir not exists
+        print(git_dir, 'not exists')
+        sys.exit()
+    else:
+        # update git repo
+        os.system('cd {} && git pull'.format(git_dir))
