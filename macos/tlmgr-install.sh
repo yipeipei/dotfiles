c50dc22469694b746459be3feb98b1e0fed1aa9d
YI Peipei <yipeipei@gmail.com>
2017-11-16 17:24:34 +0800

    install latex packages

---
+++ macos/tlmgr-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo tlmgr update --self
+sudo tlmgr install latexmk
+sudo tlmgr install helvetic  # ! I can't find file `phvb8t'.
+sudo tlmgr install enumitem  # ! LaTeX Error: File `enumitem.sty' not found.
+sudo tlmgr install kantlipsum  # ! LaTeX Error: File `kantlipsum.sty' not found.
+sudo tlmgr install preprint  # ! LaTeX Error: File `balance.sty' not found.
+sudo tlmgr install comment  # ! LaTeX Error: File `comment.sty' not found.
+sudo tlmgr install subfigure  # ! LaTeX Error: File `subfigure.sty' not found.
+sudo tlmgr install savesym  # ! LaTeX Error: File `savesym.sty' not found.
+sudo tlmgr install multirow  # ! LaTeX Error: File `multirow.sty' not found.
+sudo tlmgr install soul  # ! LaTeX Error: File `soul.sty' not found.
+sudo tlmgr install algorithmicx  # ! LaTeX Error: File `algorithmicx.sty' not found.
+sudo tlmgr install algorithms  # ! LaTeX Error: File `algorithm.sty' not found.
