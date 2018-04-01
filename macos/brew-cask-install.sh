64e2745b111e5046e043fee859363e92cde70dd6
YI Peipei <yipeipei@gmail.com>
2018-04-02 01:10:09 +0800

    add AppGrid

--- macos/brew-cask-install.sh
+++ macos/brew-cask-install.sh
@@ -6,6 +6,7 @@ brew cask install quitter
 brew cask install appcleaner
 brew cask install macpass
 brew cask install iina
+brew cask install appgrid
 brew cask install visual-studio-code
 # brew cask install dropbox
 # brew cask install dash
