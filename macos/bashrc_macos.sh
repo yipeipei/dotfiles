fd8f1037bb8db3e782a0dcd4f2634c16da31e286
YI Peipei <yipeipei@gmail.com>
2020-02-05 10:47:51 +0800

    add Homebrew's sbin to PATH

--- macos/bashrc_macos.sh
+++ macos/bashrc_macos.sh
@@ -4,6 +4,7 @@ export LANG=en_US.UTF-8
 
 # Homebrew
 export HOMEBREW_NO_ANALYTICS=true
+export PATH="/usr/local/sbin:$PATH"
 
 # autojump
 [ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
