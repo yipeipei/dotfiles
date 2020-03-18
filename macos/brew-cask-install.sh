fc7e1f344fac822cf132f775cd0d2f493a1cbfd2
YI Peipei <yipeipei@gmail.com>
2020-03-18 17:46:24 +0800

    add keycastr

--- macos/brew-cask-install.sh
+++ macos/brew-cask-install.sh
@@ -11,5 +11,6 @@ brew cask install visual-studio-code
 brew cask install anki
 brew cask install google-chrome
 brew cask install snipaste
+brew cask install keycastr
 # brew cask install dropbox
 # brew cask install dash
